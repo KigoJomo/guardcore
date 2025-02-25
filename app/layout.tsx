5fb415869c0d3c1f3c23c8290bdf6ff8028bf05d
KigoJomo <kigojomo@gmail.com>
2025-02-25 04:53:40 +0300

    Add Open Graph and Twitter metadata for enhanced social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ export const metadata: Metadata = {
   title: "Guardcore Technologies | Leading Electro-Mechanical, Telecom & IT Solutions in Kenya",
   description:
     "Discover top-tier electro-mechanical engineering, telecommunications, security systems, and IT solutions at Guardcore Technologies. Enhance your business performance with our certified professionals.",
+  openGraph: {
+    title: "Guardcore Technologies | Leading Solutions",
+    description: "Discover top-tier electro-mechanical engineering, telecommunications, security systems, and IT solutions at Guardcore Technologies. Enhance your business performance with our certified professionals.",
+    images: "/images/opengraph-image.webp",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@guardcore_tech",
+    title: "Guardcore Technologies | Leading Solutions",
+    description: "Explore our top-tier solutions and enhance your business performance.",
+    images: "/images/twitter-image.webp",
+  }
 };
 
 export default function RootLayout({
